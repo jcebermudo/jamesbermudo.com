42faa8e732c8e5f516b154257bef99d66dc39032
James Bermudo <60259094+jcebermudo@users.noreply.github.com>
2022-06-17 12:23:18 +0800

    initial

--- components/Name.jsx
+++ components/Name.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const Name = () => {
     return (
-        <Link href="/"><a><h1 className="bg-gradient-to-r bg-clip-text text-transparent from-white to-[rgba(255,255,255,0.15)] animate-text text-xl font-semibold opacity-80 select-none">James Bermudo</h1></a></Link>
+        <Link href="/"><a><h1 className="bg-gradient-to-r bg-clip-text text-transparent from-white to-[rgba(255,255,255,0.15)] animate-text text-base font-semibold opacity-80 select-none">James Bermudo</h1></a></Link>
     )
 }
 
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link'
 import Layout from '../components/Layout'
 
 export default function Home() {
@@ -13,11 +14,11 @@ export default function Home() {
         <meta name="author" content="James Bermudo" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-      <p className='text-[#A4A3A5] text-base mt-14'>I'm a Filipino designer who loves building digital products with delightful experiences. Currently, I work on freelance personal projects all while finishing high school as a 15 year old.<br /><br /> This is my little corner of the internet. You'll see the things I've made along with <a href="/writing" className='text-[#D1D1D2] duration-150 hover:text-indigo-300 underline'>essays</a> about my thoughts and experiments. I'm always active on <a href="https://twitter.com/jcebermudo" className='text-[#D1D1D2] duration-150 hover:text-indigo-300 underline'>Twitter</a> so if you'd like to chat, slide in my dms.</p>
+      <p className='text-[#A4A3A5] text-base mt-14'>I&apos;m a Filipino designer who loves building digital products with delightful experiences. Currently, I work on freelance personal projects all while finishing high school as a 15 year old.<br /><br /> This is my little corner of the internet. You&apos;ll see the things I&apos;ve made along with <Link href="/writing"><a className='text-[#D1D1D2] duration-150 hover:text-indigo-300 underline'>essays</a></Link> about my thoughts and experiments. I&apos;m always active on <a href="https://twitter.com/jcebermudo" className='text-[#D1D1D2] duration-150 hover:text-indigo-300 underline'>Twitter</a> so if you&apos;d like to chat, slide in my dms.</p>
       <div className="flex space-x-4 mt-7">
-        <a target="_blank" rel="noopener noreferrer" href="mailto:bermudo.elumba@gmail.com" className='text-[#D1D1D2] text-base duration-150 hover:text-indigo-300'><span className='underline'>Email</span>↗</a>
-        <a href="https://twitter.com/jcebermudo" target="_blank" className='text-[#D1D1D2] text-base duration-150 hover:text-indigo-300'><span className='underline'>Twitter</span>↗</a>
-        <a href="https://github.com/jcebermudo" target="_blank" className='text-[#D1D1D2] text-base duration-150 hover:text-indigo-300'><span className='underline'>GitHub</span>↗</a>
+        <a target="_blank" rel="noreferrer" href="mailto:bermudo.elumba@gmail.com" className='text-[#D1D1D2] text-base duration-150 hover:text-indigo-300'><span className='underline'>Email</span>↗</a>
+        <a href="https://twitter.com/jcebermudo" target="_blank" rel="noreferrer" className='text-[#D1D1D2] text-base duration-150 hover:text-indigo-300'><span className='underline'>Twitter</span>↗</a>
+        <a href="https://github.com/jcebermudo" target="_blank" rel="noreferrer" className='text-[#D1D1D2] text-base duration-150 hover:text-indigo-300'><span className='underline'>GitHub</span>↗</a>
       </div>
       <hr className='mt-7 border-[#767677] opacity-30'/>
       <h2 className='text-[#D1D1D2] text-base font-semibold mt-7'>Projects</h2>

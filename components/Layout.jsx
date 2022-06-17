008a478947e3beeb27d7c6a9a05c66d50360ad89
James Bermudo <60259094+jcebermudo@users.noreply.github.com>
2022-06-17 15:03:05 +0800

    min-h-screen

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -5,7 +5,7 @@ import Footy from './Footy'
 const Layout = ({children}) => {
     return(
     <div>
-    <div className="container bg-[#1B1A1D] pt-12 pb-24 sm:pt-24 min-h-fit mx-auto max-w-4xl selection:bg-[#d4cae9] selection:text-[#18181a]">
+    <div className="container bg-[#1B1A1D] bg-repeat-y pt-12 pb-24 sm:pt-24 min-h-screen mx-auto max-w-4xl selection:bg-[#d4cae9] selection:text-[#18181a]">
         <div className='mx-6 sm:mx-44'>
             <Nav/>
                 <main>{children}</main>

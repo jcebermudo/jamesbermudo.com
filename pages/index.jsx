b3cfc4866288417a4291692fadb3bc2446d0131f
James Bermudo <60259094+jcebermudo@users.noreply.github.com>
2022-06-18 12:49:27 +0800

    metadata change

--- pages/index.jsx
+++ pages/index.jsx
@@ -14,6 +14,10 @@ export default function Home() {
         <meta property="og:title" content="James Bermudo" />
         <meta property="og:description" content="A designer making delightful web experiences." />
         <meta property="og:site_name" content="James Bermudo" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:creator" content="jcebermudo" />
+        <meta property="twitter:title" content="James Bermudo" />
+        <meta property="twitter:description" content="A designer making delightful web experiences." />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
       <p className='text-[#A4A3A5] text-base mt-14'>I&apos;m a Filipino designer who loves building digital products with delightful experiences. Currently, I work on freelance personal projects all while finishing high school as a 15 year old.<br /><br /> This is my little corner of the internet. You&apos;ll see the things I&apos;ve made along with <Link href="/writing"><a className='text-[#D1D1D2] duration-150 hover:text-indigo-300 underline'>essays</a></Link> about my thoughts and experiments. I&apos;m always active on <a href="https://twitter.com/jcebermudo" target="_blank" rel="noreferrer" className='text-[#D1D1D2] duration-150 hover:text-indigo-300 underline'>Twitter</a> so if you&apos;d like to chat, slide in my dms.</p>

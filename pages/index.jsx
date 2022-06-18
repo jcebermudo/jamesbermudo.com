531634f4c1825bee6af3a04ecac4a23189a5d9c2
James Bermudo <60259094+jcebermudo@users.noreply.github.com>
2022-06-18 13:08:01 +0800

    changed metadata

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,16 +7,18 @@ export default function Home() {
   return (
       <>
       <Head>
+        <meta charSet='utf-8'/>
         <title>James Bermudo</title>
-        <link rel="icon" type="image/x-icon" href="/favicon.png"></link>
-        <meta name="description" content="A designer making delightful web experiences." />
+        <meta name="robots" content="index,follow" />
+        <meta name="description" content="A Filipino designer." />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="James Bermudo" />
         <meta property="og:description" content="A designer making delightful web experiences." />
         <meta property="og:site_name" content="James Bermudo" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" type="image/x-icon" href="/favicon.png"></link>
     </Head>
-      <p className='text-[#A4A3A5] text-base mt-14'>I&apos;m a Filipino designer who loves building digital products with delightful experiences. Currently, I work on freelance personal projects all while finishing high school as a 15 year old.<br /><br /> This is my little corner of the internet. You&apos;ll see the things I&apos;ve made along with <Link href="/writing"><a className='text-[#D1D1D2] duration-150 hover:text-indigo-300 underline'>essays</a></Link> about my thoughts and experiments. I&apos;m always active on <a href="https://twitter.com/jcebermudo" target="_blank" rel="noreferrer" className='text-[#D1D1D2] duration-150 hover:text-indigo-300 underline'>Twitter</a> so if you&apos;d like to chat, slide in my dms.</p>
+      <p className='text-[#A4A3A5] text-base mt-14'>I&apos;m a Filipino designer who loves crafting digital products. Currently, I work on freelance personal projects all while finishing high school as a 15 year old.<br /><br /> This is my little corner of the internet. You&apos;ll see the things I&apos;ve made along with <Link href="/writing"><a className='text-[#D1D1D2] duration-150 hover:text-indigo-300 underline'>essays</a></Link> about my thoughts and experiments. I&apos;m always active on <a href="https://twitter.com/jcebermudo" target="_blank" rel="noreferrer" className='text-[#D1D1D2] duration-150 hover:text-indigo-300 underline'>Twitter</a> so if you&apos;d like to chat, slide in my dms.</p>
       <div className="flex space-x-4 mt-7">
         <a target="_blank" rel="noreferrer" href="mailto:bermudo.elumba@gmail.com" className='group text-[#D1D1D2] underline text-base duration-150 hover:text-indigo-300'>
           Email

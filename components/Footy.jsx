d35e3e3206f380814d7fcf8dff0dd9db711ef5d0
James Bermudo <60259094+jcebermudo@users.noreply.github.com>
2022-06-18 18:04:01 +0800

    Font size on footer + mdx changes

--- components/Footy.jsx
+++ components/Footy.jsx
@@ -3,8 +3,8 @@ const Footy = () => {
     <div className="mt-14">
     <hr className='border-[#767677] border-dashed opacity-30'/>
     <div className="flex flex-row mt-7">
-    <p className='basis-11/12 text-[#767677] font-medium text-base select-none'>© 2022 James Bermudo</p>
-    <a target="_blank" rel="noreferrer" href="https://github.com/jcebermudo/bermudo.me/tree/main" className="basis-1/12 text-[#767677] font-medium text-base select-none duration-150 hover:text-indigo-300">source</a>
+    <p className='basis-11/12 text-[#767677] font-medium text-sm select-none'>© 2022 James Bermudo</p>
+    <a target="_blank" rel="noreferrer" href="https://github.com/jcebermudo/bermudo.me/tree/main" className="basis-1/12 text-[#767677] font-medium text-sm select-none duration-150 hover:text-indigo-300">source</a>
     </div>
     </div>
     )

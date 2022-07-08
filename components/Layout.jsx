aacf692438ffce647b7b2eed402ed8136e462291
James Bermudo <60259094+jcebermudo@users.noreply.github.com>
2022-07-08 16:38:21 +0800

    changed highlight color

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -5,7 +5,7 @@ import Footy from "./Footy";
 const Layout = ({ children }) => {
   return (
     <div>
-      <div className="container p-8 mx-auto max-w-lg sm:px-6 sm:py-24 selection:bg-[rgba(0,0,0,0.75)] selection:text-[#ffffff]">
+      <div className="container p-8 mx-auto max-w-lg sm:px-6 sm:py-24 selection:bg-[rgba(0,0,0,1)] selection:text-[#ffffff]">
         <div>
           <Nav />
           <main>{children}</main>

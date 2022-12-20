fdcbe33787f6332d0affc358d1f71e42eee2ee5a
James Bermudo <JamesTech718@gmail.com>
2022-12-20 16:02:54 +0800

    abdaoba

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -5,7 +5,7 @@ import Footy from "./Footy";
 const Layout = ({ children }) => {
   return (
     <div>
-      <div className="py-10 px-5 sm:py-24 sm:px-0 mx-auto max-w-xl">
+      <div className="py-10 px-5 sm:py-32 sm:px-0 mx-auto max-w-xl">
         <Nav />
         <main>{children}</main>
         <Footy />

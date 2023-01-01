fbc06c4877c6ac2f4278f229fbfa2c4a6a2e51c9
James <jamestech718@gmail.com>
2023-01-02 01:42:35 +0800

    minor changes

--- components/PostLink.jsx
+++ components/PostLink.jsx
@@ -5,9 +5,9 @@ function PostLink({ title, date, slug, image, description }) {
   return (
     <div>
       <Link href="posts/[slug]" as={`posts/${slug}`}>
-        <div className="group duration-150 mt-8">
+        <div className="duration-150 mt-8">
           <p className="text-[#87879A] mt-2">{date}</p>
-          <h2 className=" group-hover:text-[#1281E1] text-2xl font-medium">
+          <h2 className="text-2xl font-medium">
             {title}
           </h2>
           {Boolean(image) && (
@@ -21,7 +21,7 @@ function PostLink({ title, date, slug, image, description }) {
           )}
           <p className="mt-7">
             {description}
-            <span className="font-bold">...Read more</span>
+            <span className="font-bold duration-75 hover:text-[#1281E1]">...Read more</span>
           </p>
         </div>
       </Link>

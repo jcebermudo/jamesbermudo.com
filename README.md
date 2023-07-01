04b4f2b523c1710dd4d493513b290152c2b8016f
James <jamestech718@gmail.com>
2023-07-01 15:26:03 +0800

    clean

--- README.md
+++
@@ -1,1 +0,0 @@
-I built this website as a space to store all of my work and thoughts.
--- components/ImgText.jsx
+++
@@ -1,9 +0,0 @@
-const ImgText = ({ text }) => {
-  return (
-    <div className="flex flex-row items-center justify-center">
-      <p className="italic text-center opacity-50 my-0">{text}</p>
-    </div>
-  );
-};
-
-export default ImgText;
